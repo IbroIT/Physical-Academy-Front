a27207dbcbcc6c7840a89b409488b57f1fc4e546
Ibro <ibroabdraimov20@gmail.com>
2025-11-08 13:57:12 +0600

    asdfa

--- src/components/Privacy.jsx
+++ src/components/Privacy.jsx
@@ -60,7 +60,6 @@ const PrivacyPolicy = () => {
         <div className="absolute top-40 right-10 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-20 animate-pulse animation-delay-2000"></div>
         <div className="absolute bottom-10 left-20 w-72 h-72 bg-cyan-300 rounded-full mix-blend-multiply filter blur-xl opacity-20 animate-pulse animation-delay-4000"></div>
       </div>
-
       <div ref={ref} className="relative z-10 py-20 px-4">
         <motion.div
           variants={containerVariants}
